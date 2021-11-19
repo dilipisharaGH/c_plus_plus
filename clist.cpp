ad7683c7fbb6090df845b03325e9860c40463ffd
Dilip ishara <94555727+dilipisharaGH@users.noreply.github.com>
2021-11-19 16:07:50 +0530

    Update clist.cpp

--- clist.cpp
+++ clist.cpp
@@ -2,11 +2,11 @@
 clist [THE ARRAY HANDLER!]
 
 I create this data structure to enhance the performance of the C Plus Plus array and make it more efficient.
-you can use clist_guide_viwe() function for know about syntax rules of my data structure.
-also you can use my data structure as libray whether you want with save it under .hpp file extension!
+this array handler has clear syntax rules, and it easy to use than vectors.
+also you can use my data structure as libray whether you want with save it under the .hpp or .h file extension!
 i invite you to change this data structure and improve its skills than now!
 
-i hope to improve skills of this data structure
+i hope to improve skills of this data structure, and upgrade its perfomance monthly.
 contact dilipishara52@gmail.com or +94704532646 for send suggestion and further information
 thanks for using my "clist ARRAY HADLER"
 */
